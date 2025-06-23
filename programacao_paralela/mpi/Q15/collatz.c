b3b28e4371944e82300fe345b81230a018f55e7e
JhoanDev <jhoandev.log@gmail.com>
2025-06-23 18:26:30 -0300

    refact: Melhoria na sincronia

--- programacao_paralela/mpi/Q15/collatz.c
+++ programacao_paralela/mpi/Q15/collatz.c
@@ -6,6 +6,10 @@
 #define comm MPI_COMM_WORLD
 #define MAX 100000000
 
+#define TAG_TAREFA 1
+#define TAG_RESULTADO 2
+#define TAG_FIM 3
+
 int collatz_passos(int n)
 {
     int passos = 0;
@@ -24,14 +28,13 @@ int collatz_passos(int n)
 
 int main(int argc, char *argv[])
 {
-    int my_rank, comm_sz, tag_tarefa = 0, tag_num = 1, tag_passos = 2;
+    int my_rank, comm_sz;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(comm, &my_rank);
     MPI_Comm_size(comm, &comm_sz);
     double start_time, end_time, min_time, max_time;
 
     MPI_Status status;
-    MPI_Request req_envio1, req_envio2, req_recebe;
 
     MPI_Barrier(comm);
     start_time = MPI_Wtime();
@@ -39,40 +42,35 @@ int main(int argc, char *argv[])
     if (my_rank == 0)
     {
         int num_enviado = 1;
-        int num_recebido, passos_recebidos;
         int maior_passos = 0;
         int num_com_maior = 0;
+        int resultado_recebido[2]; // [0] = número, [1] = passos
 
         for (int proc = 1; proc < comm_sz && num_enviado <= MAX; proc++, num_enviado++)
         {
-            MPI_Isend(&num_enviado, 1, MPI_INT, proc, tag_tarefa, comm, &req_envio1);
-            MPI_Wait(&req_envio1, MPI_STATUS_IGNORE);
+            MPI_Send(&num_enviado, 1, MPI_INT, proc, TAG_TAREFA, comm);
         }
 
         int processos_ativos = comm_sz - 1;
         while (processos_ativos > 0)
         {
-            MPI_Recv(&num_recebido, 1, MPI_INT, MPI_ANY_SOURCE, tag_num, comm, &status);
-            MPI_Recv(&passos_recebidos, 1, MPI_INT, status.MPI_SOURCE, tag_passos, comm, MPI_STATUS_IGNORE);
+            MPI_Recv(resultado_recebido, 2, MPI_INT, MPI_ANY_SOURCE, TAG_RESULTADO, comm, &status);
 
-            if (passos_recebidos > maior_passos)
+            if (resultado_recebido[1] > maior_passos)
             {
-                maior_passos = passos_recebidos;
-                num_com_maior = num_recebido;
+                maior_passos = resultado_recebido[1];
+                num_com_maior = resultado_recebido[0];
             }
 
             if (num_enviado <= MAX)
             {
-                MPI_Isend(&num_enviado, 1, MPI_INT, status.MPI_SOURCE, tag_tarefa, comm, &req_envio1);
+                MPI_Send(&num_enviado, 1, MPI_INT, status.MPI_SOURCE, TAG_TAREFA, comm);
                 num_enviado++;
-                MPI_Wait(&req_envio1, MPI_STATUS_IGNORE);
             }
             else
             {
-                int fim = -1;
-                MPI_Isend(&fim, 1, MPI_INT, status.MPI_SOURCE, tag_tarefa, comm, &req_envio1);
+                MPI_Send(NULL, 0, MPI_INT, status.MPI_SOURCE, TAG_FIM, comm);
                 processos_ativos--;
-                MPI_Wait(&req_envio1, MPI_STATUS_IGNORE);
             }
         }
 
@@ -80,23 +78,29 @@ int main(int argc, char *argv[])
     }
     else
     {
-        while (1)
+        int tarefa_atual;
+        int tarefa_proxima;
+        int resultado_anterior[2]; // {numero, passos}
+        MPI_Request req_envio_resultado, req_recebe_tarefa;
+
+        MPI_Recv(&tarefa_atual, 1, MPI_INT, 0, TAG_TAREFA, comm, &status);
+
+        while (status.MPI_TAG != TAG_FIM)
         {
-            int numero;
-            MPI_Irecv(&numero, 1, MPI_INT, 0, tag_tarefa, comm, &req_recebe);
-            MPI_Wait(&req_recebe, MPI_STATUS_IGNORE);
+            MPI_Irecv(&tarefa_proxima, 1, MPI_INT, 0, MPI_ANY_TAG, comm, &req_recebe_tarefa);
 
-            if (numero == -1)
-                break;
+            int passos = collatz_passos(tarefa_atual);
+            resultado_anterior[0] = tarefa_atual;
+            resultado_anterior[1] = passos;
 
-            int passos = collatz_passos(numero);
+            MPI_Isend(resultado_anterior, 2, MPI_INT, 0, TAG_RESULTADO, comm, &req_envio_resultado);
 
-            MPI_Isend(&numero, 1, MPI_INT, 0, tag_num, comm, &req_envio1);
-            MPI_Isend(&passos, 1, MPI_INT, 0, tag_passos, comm, &req_envio2);
-            MPI_Wait(&req_envio1, MPI_STATUS_IGNORE);
-            MPI_Wait(&req_envio2, MPI_STATUS_IGNORE);
+            MPI_Wait(&req_recebe_tarefa, &status);
+            tarefa_atual = tarefa_proxima;
+            MPI_Wait(&req_envio_resultado, MPI_STATUS_IGNORE);
         }
     }
+
     end_time = MPI_Wtime();
     MPI_Reduce(&start_time, &min_time, 1, MPI_DOUBLE, MPI_MIN, 0, comm);
     MPI_Reduce(&end_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
