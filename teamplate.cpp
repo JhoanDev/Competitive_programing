e3dd47ef5a0a582a68a00b2fd51ce81fe504cb16
jhoanDev <jhoandev.log@gmail.com>
2026-06-07 08:21:56 -0300

    chore: configure clang-format and clangd settings and clean up includes in junina.cpp

--- teamplate.cpp
+++ teamplate.cpp
@@ -1,23 +1,46 @@
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
 #include <bits/stdc++.h>
 
 using namespace std;
-#define _ \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);
+
+// Aliases de tipo modernos (substituindo typedef)
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// Constantes avaliadas em tempo de compilação
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+// Macros úteis e otimizadas
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ssize(x)
 #define endl '\n'
-#define f first
-#define s second
 
-typedef unsigned long long llu;
-typedef unsigned int ui;
-typedef long long ll;
+void solve()
+{
+    // A lógica do seu problema começa aqui
+}
 
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+int main()
+{
+    fastio;
 
-int main(){
-    //_
+    int t = 1;
+    // cin >> t; // Descomente para problemas com múltiplos test cases
 
+    while (t--)
+    {
+        solve();
+    }
 
     return 0;
 }
